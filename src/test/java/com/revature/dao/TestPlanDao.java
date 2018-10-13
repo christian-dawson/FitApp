321c34d10939c74412ec0320d123b2cac5b18bbc
Tad Perry <tdvprr@gmail.com>
2018-10-13 18:54:05 -0400

    added fitness plan dao test

--- src/test/java/com/revature/dao/TestPlanDao.java
+++ src/test/java/com/revature/dao/TestPlanDao.java
@@ -7,6 +7,9 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
@@ -17,6 +20,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import com.revature.dao.impl.FitnessPlanDaoImpl;
 import com.revature.dao.impl.UserDaoImpl;
 import com.revature.pojos.FitnessPlan;
 import com.revature.pojos.User;
@@ -37,68 +41,72 @@ public class TestPlanDao {
 	@Before
 	public void beforeTest() {
 		MockitoAnnotations.initMocks(this);
-		users = new ArrayList<>();
+		testPlans = new ArrayList<>();
 		for(int i = 0; i < 10; i++) {
-			User toAdd = new User();
-			toAdd.setId(i);
-			users.add(toAdd);
-		}
-		testUser = new User();
-	    testUser.setAddress("a;lskdfja fa sklfdja;sklfd");
-	    testUser.setBirthdate(LocalDate.now());
-	    testUser.setFirstName("a");
-	    testUser.setLastName("laskdfj");
-	    testUser.setHeight(1290);
-	    testUser.setId(1923);
-	    testUser.setUsername("laksfdjs");
-	    testUser.setPassword("alskdjf");
-	    testUser.setType("trainer");
-	    testUser.setWeight(9234023);
-	    dao = new UserDaoImpl();
+			FitnessPlan toAdd = new FitnessPlan();
+			toAdd.setPlanId(i);
+			testPlans.add(toAdd);
+		}	    
+		Integer planId = 14;
+		Integer traineeId = 11;
+		Integer trainerId = 12;
+		Integer dietId = 13;
+		Integer mWorkout = 12;
+		Integer tuWorkout = 74;
+		Integer wWorkout = 34;
+		Integer thWorkout = 43;
+		Integer fWorkout = 2;
+		Integer saWorkout = 12;
+		Integer suWorkout =23;
+		testPlan = new FitnessPlan(planId, traineeId, trainerId, dietId, mWorkout, tuWorkout, wWorkout,
+				thWorkout, fWorkout, saWorkout, suWorkout);
+		
+	    dao = new FitnessPlanDaoImpl();
 		dao.setSession(sess);
-		(Mockito.doReturn(testUser).when(sess)).get(User.class, testUser.getId());
-		(Mockito.doReturn(query).when(sess)).createQuery("FROM User");
-		(Mockito.doReturn(users).when(query)).getResultList();
+		(Mockito.doReturn(testPlan).when(sess)).get(FitnessPlan.class, testPlan.getPlanId());
+		(Mockito.doReturn(query).when(sess)).createQuery("FROM FitnessPlan");
+		(Mockito.doReturn(testPlans).when(query)).getResultList();
 		(Mockito.doReturn(tran).when(sess)).beginTransaction();
 	}
+
 	@After
 	public void afterTest() {
-		testUser = null;
-		users.clear();
+		testPlan = null;
+		testPlans.clear();
 		dao = null;
 	}
 	
 	@Test
 	public void testRead() {
-		User result = dao.read(testUser.getId());
-		assertEquals(testUser.getId(), result.getId());
-		assertEquals(testUser.getAddress(), result.getAddress());
-		assertEquals(testUser.getBirthdate(), result.getBirthdate());
-		assertEquals(testUser.getUsername(), result.getUsername());
-		assertEquals(testUser.getWeight(), result.getWeight(), .1);
+		FitnessPlan result = dao.read(testPlan.getPlanId());
+		assertEquals(testPlan.getPlanId(), result.getPlanId());
+		assertEquals(testPlan.getDietId(), result.getDietId());
+		assertEquals(testPlan.getTraineeId(), result.getTraineeId());
+		assertEquals(testPlan.getTrainerId(), result.getTrainerId());
+		assertEquals(testPlan.getThWorkout(), result.getThWorkout());
 		
 	}
 	
 	@Test
 	public void testReadAll() {
-		assertEquals(users, dao.readAll());	
+		assertEquals(testPlans, dao.readAll());	
 	}
 	
 	@Test
 	public void testUpdate() {
-		dao.update(testUser);
-		verify(sess).update(testUser);
+		dao.update(testPlan);
+		verify(sess).update(testPlan);
 	}
 	
 	@Test
 	public void testDelete() {
-		dao.delete(testUser);
-		verify(sess).delete(testUser);
+		dao.delete(testPlan);
+		verify(sess).delete(testPlan);
 	}
 	@Test
 	public void testSave() {
-		dao.create(testUser);
-		verify(sess).save(testUser);
+		dao.create(testPlan);
+		verify(sess).save(testPlan);
 	}
 	
 }
